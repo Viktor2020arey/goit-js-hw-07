7921e71374b1735da043b8e847e6987abf34a1f2
Holovatiuk Victor <Stethem696@gmail.com>
2021-02-25 09:04:13 +0200

    ffff

---
+++ js/task-4.js
@@ -0,0 +1,15 @@
+{
+  /* <div id="counter">
+  <button type="button" data-action="decrement">
+    -1
+  </button>
+  <span id="value">0</span>
+  <button type="button" data-action="increment">
+    +1
+  </button>
+</div>; */
+}
+const counterValue = document.querySelector('#value');
+console.log(counterValue);
+// const decrementClick = document.querySelector('data - action = decrement');
+// const incrementClick = document.querySelector('data - action = increment');
