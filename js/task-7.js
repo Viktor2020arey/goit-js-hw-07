c60a140464a3a5a8930af81c5d74074883bb4148
Holovatiuk Victor <Stethem696@gmail.com>
2021-02-27 01:07:58 +0200

    Закончил 7ю задачу

--- js/task-7.js
+++ js/task-7.js
@@ -1,5 +1,9 @@
-{
-  /* <input id="font-size-control" type="range" />
-<br />
-<span id="text">Абракадабра!</span> */
+const refs = {
+  range: document.querySelector('#font-size-control'),
+  span: document.querySelector('#text'),
+};
+refs.range.addEventListener('input', onTextSpanChanger);
+
+function onTextSpanChanger(event) {
+  refs.span.style.fontSize = event.currentTarget.value + 'px';
 }
