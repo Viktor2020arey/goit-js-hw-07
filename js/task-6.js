313386b1648bd3ffb1a795d868e91493eea41720
Holovatiuk Victor <Stethem696@gmail.com>
2021-02-27 01:45:09 +0200

    исправил 6ю задачу

--- js/task-6.js
+++ js/task-6.js
@@ -3,7 +3,7 @@ const input = document.querySelector('#validation-input');
 input.addEventListener('blur', onInputBlur);
 
 function onInputBlur() {
-  if (input.value.length < input.dataset.length) {
+  if (input.value.length != input.dataset.length) {
     input.classList.add('invalid');
     input.classList.remove('valid');
   } else {
